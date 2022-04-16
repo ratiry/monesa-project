21c032139fb807ec163d0b9c27671c6a07e7567b
ratiry <ratiry@yandex.ru>
2022-04-16 11:49:50 +0300

    slider is done

--- js.js
+++ js.js
@@ -6,6 +6,7 @@ let popup_cross = document.getElementById('popup-yellow-cross');
 let types_section = document.getElementById('types');
 let types_buttons = types_section.querySelectorAll('button');
 let website_html = document.querySelector('.Website_html');
+let slide = document.querySelector('.presentation-section-slider-slide');
 for(let i=0;i<types_buttons.length;i++){
   types_buttons[i].addEventListener('click',function(){
     popup_yellow.classList.add('popup-animation');
@@ -19,3 +20,28 @@ popup_cross.addEventListener('click',function(){
   website_html.style.overflow = 'scroll';
   website_html.style.opacity = '100%';
 })
+let slideIndex = 0;
+let slider_button_next = document.getElementById('slider_button_next');
+let slider_button_previous = document.getElementById('slider_button_previous');
+slider_button_next.addEventListener('click',function(){
+  plusSlide_function(1);
+})
+slider_button_previous.addEventListener('click',function(){
+  plusSlide_function(-1);
+})
+let plusSlide_function = function(n){
+  showSlides(slideIndex +=n);
+}
+let showSlides = function(){
+  if(slideIndex > slide.children.length-1){
+    slideIndex =0;
+  }
+  if(slideIndex<0){
+    slideIndex =slide.children.length-1;
+  }
+  for(let i=0;i<slide.children.length;i++){
+    slide.children[i].style.display = 'none';
+  }
+  slide.children[slideIndex].style.display = 'block';
+}
+showSlides(slideIndex);
