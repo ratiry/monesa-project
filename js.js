748c90b03323b0f8ddf5d6b60d6948bd1785b22b
ratiry <ratiry@yandex.ru>
2022-04-18 11:59:55 +0300

    slider dots were added

--- js.js
+++ js.js
@@ -6,6 +6,7 @@ let popup_cross = document.getElementById('popup-yellow-cross');
 let types_section = document.getElementById('types');
 let types_buttons = types_section.querySelectorAll('button');
 let website_html = document.querySelector('.Website_html');
+let slider_dots_container = document.querySelector('.slider-dots');
 let slide = document.querySelector('.presentation-section-slider-slide');
 for(let i=0;i<types_buttons.length;i++){
   types_buttons[i].addEventListener('click',function(){
@@ -20,6 +21,16 @@ popup_cross.addEventListener('click',function(){
   website_html.style.overflow = 'scroll';
   website_html.style.opacity = '100%';
 })
+
+for(let i=0;i<slide.children.length;i++){
+  let button= document.createElement('button');
+  button.classList.add('slider-dot');
+  slider_dots_container.appendChild(button);
+  button.addEventListener('click',function(){
+    equel_slide_function(i);
+  })
+}
+
 let slideIndex = 0;
 let slider_button_next = document.getElementById('slider_button_next');
 let slider_button_previous = document.getElementById('slider_button_previous');
@@ -32,6 +43,9 @@ slider_button_previous.addEventListener('click',function(){
 let plusSlide_function = function(n){
   showSlides(slideIndex +=n);
 }
+let equel_slide_function = function(n){
+  showSlides(slideIndex =n);
+}
 let showSlides = function(){
   if(slideIndex > slide.children.length-1){
     slideIndex =0;
@@ -41,7 +55,9 @@ let showSlides = function(){
   }
   for(let i=0;i<slide.children.length;i++){
     slide.children[i].style.display = 'none';
+    slider_dots_container.children[i].classList.remove('active-button-slider');  
   }
+  slider_dots_container.children[slideIndex].classList.add('active-button-slider');
   slide.children[slideIndex].style.display = 'block';
 }
 showSlides(slideIndex);
