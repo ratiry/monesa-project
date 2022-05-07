d267ee4717b8e3c526e3a1ee9b2dfdd26d57c982
ratiry <ratiry@yandex.ru>
2022-05-07 18:03:09 +0300

    my_disappearing_popup

--- js.js
+++ js.js
@@ -11,12 +11,13 @@ let slider_dots_container = document.querySelector('.slider-dots');
 let slide = document.querySelector('.hero-section-slider-slide');
 
 // Popup
+let key_to_close_popup = false;
 for(let i=0;i<types_buttons.length;i++){
   types_buttons[i].addEventListener('click',function(){
     popup_yellow.classList.add('popup-animation');
     body_overlay.classList.add('visible');
     website_html.style.overflow = 'hidden';
-
+    key_to_close_popup = true;
   })
 }
 
@@ -26,6 +27,7 @@ popup_cross.addEventListener('click',function(){
   body_overlay.classList.remove('visible');
   website_html.style.overflow = 'scroll';
   website_html.style.opacity = '100%';
+  key_to_close_popup = false;
 })
 body_overlay.addEventListener('click',function(){
   popup_yellow.classList.add('display-none');
@@ -33,8 +35,19 @@ body_overlay.addEventListener('click',function(){
   body_overlay.classList.remove('visible');
   website_html.style.overflow = 'scroll';
   website_html.style.opacity = '100%';
+  key_to_close_popup = false;
 })
-
+// window.addEventListener('click',function(e){
+//   if(!e.target.closest("#popup-yellow") & window.getComputedStyle(popup_yellow).visibility == 'visible' ){
+//     console.log('something');
+//     popup_yellow.classList.add('display-none');
+//     popup_yellow.classList.remove('popup-animation');
+//     body_overlay.classList.remove('visible');
+//     website_html.style.overflow = 'scroll';
+//     website_html.style.opacity = '100%';
+//     key_to_close_popup = false;
+//   }
+// })
 // End Popup
 $('#hero_section_slider').slick({
   dots: true,
@@ -58,3 +71,5 @@ menu_appearing_button.addEventListener('click',function(){
 })
 
 // END Menu
+
+
